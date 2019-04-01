1456d711a99611402534b687a2235f6edc3a9c6d
wenewzhang <wenewboy@gmail.com>
2019-04-01 21:17:53 +0800

    add CreateUser & test

--- examples/http.js
+++ examples/http.js
@@ -89,6 +89,29 @@ console.log('Supported Endpoints by HttpClient', client.getEndpoints());
     const friends = await client.getFriends();
     const contacts = await client.getContacts();
     console.log({ profile, friends, user, users, contacts });
+
+    const { generateKeyPairSync } = require('crypto');
+    const { publicKey, privateKey } = generateKeyPairSync('rsa',
+    {   modulusLength: 1024,  // the length of your key in bits
+        publicKeyEncoding: {
+          type: 'spki',       // recommended to be 'spki' by the Node.js docs
+          format: 'pem'
+        },
+        privateKeyEncoding: {
+          type: 'pkcs1',      // recommended to be 'pkcs8' by the Node.js docs
+          format: 'pem',
+          //cipher: 'aes-256-cbc',   // *optional*
+          //passphrase: 'top secret' // *optional*
+      }
+    });
+    publicKey1 = publicKey.replace("-----BEGIN PUBLIC KEY-----","");
+    publicKey2 = publicKey1.replace("-----END PUBLIC KEY-----","");
+    publicKey3 = publicKey2.replace(/\r?\n|\r/g, "");
+
+    const info = await client.createUser({full_name : "bitcoin wallet",
+                       session_secret: publicKey3,
+                     });
+    console.log(info);
   } catch (err) {
     console.error('error', err);
     console.trace(err);
